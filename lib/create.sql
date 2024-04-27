fb4903e3c83b0ea4412298e4f4a46a786c82f025
RosaliaNjoki <njosali0@gmail.com>
2024-04-28 01:29:24 +0300

    done

---
+++ lib/create.sql
@@ -0,0 +1,8 @@
+CREATE TABLE bears (id INTEGER,
+name TEXT, 
+age INTEGER, 
+sex TEXT, 
+color TEXT, 
+temperature TEXT, 
+alive BOOLEAN 
+);
---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (name, age, sex, color, temperature, alive)
+VALUES ('Mr. Chocolate', 20, 'male', 'black', '72.8', true), 
+('Rowdy', 14, 'male', 'brown', '74.5', true), 
+('Tabitha', 12, 'female', 'white', '77.2', true), 
+('Sergent Betown', 15, 'male', 'brown', '76.5', true), 
+('Melissa', 16, 'female', 'yellow', '69.9', true), 
+('Grinch', 2, 'male', 'black', '78.5', true), 
+('Wendy', 18, 'female', 'white', '69.9', true), 
+('unnamed', 1, 'male', 'brown', '77.2', false);
